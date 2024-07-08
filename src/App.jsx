3232939c602931097c1b315ab11e42f20237fb24
grumpiestcub <grumpiestcub@gmail.com>
2024-07-08 18:11:11 -0500

    attempted to add basic template styling to player and still not able to get it to appear

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header"
 import Buttons from "./components/Buttons"
-// import Player from "./components/Player" why does importing something without using it break the localhost? Feels like it should just ignore it?
+// import Player from "./components/Player" commenting this out so the localhost displays
 import './App.css'
 
 // should I be seperating this out even further? For example multiple components instead of just Header? 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Buttons />
-      {/* <Player /> this isn't showing anything? maybe because no CSS? Even commented out I now just have a white screen for the localhost */}
+      {/* <Player /> */}
     </div>
   )
 }
